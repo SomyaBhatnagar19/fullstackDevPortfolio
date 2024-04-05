39449964de96b25d27590bd4f882f360e1fb18c7
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-04-05 13:29:04 +0530

    Header or menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,11 +1,30 @@
 /* /views/pages/menu.js */
 
 import React from 'react'
+import './css/menu.css';
+import {Container, Nav, Navbar, Image} from 'react-bootstrap';
+import menuLogo from '../assets/menuLogo.png';
 
 function Menu() {
   return (
-    <div>menu</div>
-  )
+    <Navbar collapseOnSelect expand="md">
+      <Container>
+        <Navbar.Brand href="#home"><Image src={menuLogo} alt='menu-logo' className='menu-logo'/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#aboutMe">About Me</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#project">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#Resume">Download Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Menu;
