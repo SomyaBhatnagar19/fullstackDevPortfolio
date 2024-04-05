6936b03836f4754bfc6dc37d34906c8613fbfb11
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-04-05 11:40:13 +0530

    Making the home component.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,33 @@
-/* /views/pages/home.js */
+/* /pages/home.js */
 
-import React from 'react'
+import React from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import "./css/home.css";
+import womenWorking from "../assets/womenWorking.png";
 
 function Home() {
   return (
-    <div>home</div>
-  )
+    <div className="home-div">
+      <Container>
+      <Row className="home">
+        <Col xs={12} md={6}>
+          <div className="text-content">
+            <h3 className="greeting">Hi! I'm </h3>
+            <h1 className="name">Somya Bhatnagar.</h1>
+            <h3 className="title">I'm a Fullstack Developer.</h3>
+          </div>
+        </Col>
+        <Col xs={12} md={6}>
+          <Image
+            src={womenWorking}
+            alt="home-illustration"
+            className="home-image img-fluid"
+          />
+        </Col>
+      </Row>
+    </Container>
+    </div>
+  );
 }
 
 export default Home;
