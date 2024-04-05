25a4c39f4082e71fed3300a92959c3b033f6f646
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-04-05 16:32:59 +0530

    About me component and styling.

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -1,7 +1,28 @@
+/* /pages/css/aboutMe.js */
+
+import aboutMe from "../assets/aboutMe.png";
+import { Container, Image, Row, Col, Button } from "react-bootstrap";
+import './css/aboutMe.css'
+
 function AboutMe() {
-    return (
-      <div>aboutMe</div>
-    )
-  }
-  
-  export default AboutMe;
+  return (
+    <div className="aboutMe-div">
+      <Container>
+        <Row>
+            <h2 className="aboutMe-title">About Me</h2>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <Image src={aboutMe} alt="About Me" id="aboutMe-image" className="aboutMe-image img-fluid animate__animated animate__fadeInLeft"/>
+          </Col>
+          <Col xs={12} md={6}>
+            <p id="aboutMe-content" className="aboutMe-image img-fluid animate__animated animate__zoomIn">Hi, I'm Somya, a full-stack web developer and software engineer from Delhi. I'm passionate about exploring new technologies and using my creativity to build exciting projects. I thrive in team environments, finding collaborative work incredibly rewarding. I enjoy the challenge of solving problems and crafting user-friendly interfaces. My goal is to create meaningful and impactful digital experiences. Let's collaborate and create something awesome together! If you're looking for a dedicated and skilled developer, I'd love to discuss how I can contribute to your team.</p>
+            <Button className="resume">My Resume</Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default AboutMe;
