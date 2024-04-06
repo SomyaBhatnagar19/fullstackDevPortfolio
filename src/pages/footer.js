8f9864f81753fe4ca732e2a025405919b5f34db0
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-04-06 12:24:27 +0530

    Making the footer.

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,11 +1,27 @@
-/* /views/pages/footer.js */
+/* /views/components/Footer.js */
 
-import React from 'react'
+import React from 'react';
+import { FaChevronUp, FaGithub, FaLinkedin } from 'react-icons/fa';
+import './css/footer.css';
 
 function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
-    <div>footer</div>
-  )
+    <footer className="footer">
+      <div className="footer-rights">© {new Date().getFullYear()} Rights Reserved to Somya Bhatnagar</div>
+      <div className="footer-social">
+      <a href="https://github.com/somya1403" target="_blank" rel="noopener noreferrer"><FaGithub style={{ color: '#020617' }} /></a>
+        <a href="https://linkedin.com/in/somya-bhatnagar" target="_blank" rel="noopener noreferrer"><FaLinkedin style={{ color: '#1d4ed8' }} /></a>
+      </div>
+      <button className="btn btn-primary btn-scroll-top" onClick={scrollToTop}><FaChevronUp /></button>
+    </footer>
+  );
 }
 
 export default Footer;
