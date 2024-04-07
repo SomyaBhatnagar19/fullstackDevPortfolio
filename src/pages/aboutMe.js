dfe7d1bee9093125010aba93d8727042c8e75ddf
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-04-08 00:10:20 +0530

    Adding resume pdf to the button in about me component.

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -3,6 +3,7 @@
 import aboutMe from "../assets/aboutMe.png";
 import { Container, Image, Row, Col, Button } from "react-bootstrap";
 import './css/aboutMe.css'
+import resume from '../assets/resume.pdf';
 
 function AboutMe() {
   return (
@@ -17,7 +18,11 @@ function AboutMe() {
           </Col>
           <Col xs={12} md={6}>
             <p id="aboutMe-content" className="aboutMe-image img-fluid animate__animated animate__zoomIn">Hi, I'm Somya, a full-stack web developer and software engineer from Delhi. I'm passionate about exploring new technologies and using my creativity to build exciting projects. I thrive in team environments, finding collaborative work incredibly rewarding. I enjoy the challenge of solving problems and crafting user-friendly interfaces. My goal is to create meaningful and impactful digital experiences. Let's collaborate and create something awesome together! If you're looking for a dedicated and skilled developer, I'd love to discuss how I can contribute to your team.</p>
-            <Button className="resume">My Resume</Button>
+            <Button className="resume">
+  <a href={resume} download="resume">
+    My Resume</a>
+</Button>
+
           </Col>
         </Row>
       </Container>
